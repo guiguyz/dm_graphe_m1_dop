b0b6c769fa3185f768c23a90afebf33b1f42ec38
Guillaume Drouart <guiguyz@hotmail.fr>
2017-10-30 10:37:42 +0100

    Test pour parser un fichier tsp

--- Graphe/src/Matrice.java
+++ Graphe/src/Matrice.java
@@ -1,44 +1,50 @@
 
-
 /**
  * Represente une matrice [i,j] symetrique trianglulaire supperieur
  * (i,j)=(j,i)
- * @author 
+ *
+ * @author
  */
 public class Matrice<T extends Object> {
 
-	private Object matrice[][];
-	public final int length;
-	public Matrice(final Ville[] villes) {
-		this(villes, null);
-	}
-	public Matrice(final Ville[] villes, final T defaultValue) {
-		length = villes.length;
-		matrice = new Object[length][length];
-		for (int i = 0; i < length; i++)
-			matrice[i] = new Object[length-i];
-		
-		resetAll(defaultValue);
-	}
-	public  void resetAll(final T defaultValue){
-		for (int i = 0; i < length; i++) {
-			for (int j = i; j < length; j++) {
-				matrice[i][j - i] = defaultValue;
-			}
-		}	
-	}
-	public void set(final int i,final int j, final T value){
-		final int i1 = Math.min(i, j);
-		final int j1 = Math.max(i, j);
-		matrice[i1][j1 - i1] = value;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public T get(final int i, final int j){
-		final int i1 = Math.min(i, j);
-		final int j1 = Math.max(i, j);
-		if(matrice[i1][j1 - i1] == null)
-			return null;
-		return (T)matrice[i1][j1 - i1];
-	}
+    private Object matrice[][];
+    public final int length;
+
+    public Matrice(final Ville[] villes) {
+        this(villes, null);
+    }
+
+    public Matrice(final Ville[] villes, final T defaultValue) {
+        length = villes.length;
+        matrice = new Object[length][length];
+        for (int i = 0; i < length; i++) {
+            matrice[i] = new Object[length - i];
+        }
+
+        resetAll(defaultValue);
+    }
+
+    public void resetAll(final T defaultValue) {
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j < length; j++) {
+                matrice[i][j - i] = defaultValue;
+            }
+        }
+    }
+
+    public void set(final int i, final int j, final T value) {
+        final int i1 = Math.min(i, j);
+        final int j1 = Math.max(i, j);
+        matrice[i1][j1 - i1] = value;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T get(final int i, final int j) {
+        final int i1 = Math.min(i, j);
+        final int j1 = Math.max(i, j);
+        if (matrice[i1][j1 - i1] == null) {
+            return null;
+        }
+        return (T) matrice[i1][j1 - i1];
+    }
 }
--- Graphe/src/Tsp.java
+++ Graphe/src/Tsp.java
@@ -1,57 +1,59 @@
+
 import java.text.DecimalFormat;
 
 /**
  * Relaxation lagrangienne et voyageur de commerce
+ *
  * @author Constantin Laurent
  * @author Gander Jonathan
  */
-
 /**
  *
- * @author 
+ * @author
  */
 public class Tsp {
-/**
-	 * Reffectue la relaxion lagrangienne sur un graph complet.
-	 * La pondreation du graph est donne par les distances entre villes.
-	 * @param villes Villes du graph.
-	 * @param la tournee
-	 */
-	public static Ville[] relaxation(Ville[] villes) {
 
-		// Choix d'alpha
-		//double alpha = 1.9;
+    /**
+     * Reffectue la relaxion lagrangienne sur un graph complet. La pondreation
+     * du graph est donne par les distances entre villes.
+     *
+     * @param villes Villes du graph.
+     * @param la tournee
+     */
+    public static Ville[] relaxation(Ville[] villes) {
 
-		// Initialise un 1-arbre
-		Arbre arbre = new Arbre(villes);
+        // Choix d'alpha
+        //double alpha = 1.9;
+        // Initialise un 1-arbre
+        Arbre arbre = new Arbre(villes);
 
-		// Initalisation de la borne
-		double borne = arbre.bornePonderee();
-		double ancienneBorne = borne;
+        // Initalisation de la borne
+        double borne = arbre.bornePonderee();
+        double ancienneBorne = borne;
 
-		// La mise a jour ne se fait pas a la premiere iteration
-		boolean premiereIteration = true;
+        // La mise a jour ne se fait pas a la premiere iteration
+        boolean premiereIteration = true;
 
-		// Indique si l'arbre est une tournee
-		boolean estUneTournee = false;
+        // Indique si l'arbre est une tournee
+        boolean estUneTournee = false;
 
-		// Affichage
-		DecimalFormat df = new DecimalFormat("########.00");
+        // Affichage
+        DecimalFormat df = new DecimalFormat("########.00");
 
-		do {
-			// Des la deuxieme iteration
-			if (!premiereIteration) {
+        do {
+            // Des la deuxieme iteration
+            if (!premiereIteration) {
 
-				// Met a jour le 1-arbre, 
-				arbre.miseAJour();
+                // Met a jour le 1-arbre, 
+                arbre.miseAJour();
 
-				// Calcule de la nouvelle borne
-				ancienneBorne = borne;
-				borne = arbre.bornePonderee();
-			}
+                // Calcule de la nouvelle borne
+                ancienneBorne = borne;
+                borne = arbre.bornePonderee();
+            }
 
-			// Mise a jour des lambdas
-			/*
+            // Mise a jour des lambdas
+            /*
 			 * TODO Implementer la consigne
 			 * double somme = 0.0;
 			 * for (int i = 0; i < villes.length; i++) {
@@ -62,76 +64,89 @@ public class Tsp {
 			 * System.err.println("Erreur de somme");
 			 * somme = 1;
 			 * }
-			 */
-
-			// Calcule du nouveau lambda pour tous les sommets
-			for (int i = 0; i < villes.length; i++) {
-				/* TODO  Implementer la consigne
+             */
+            // Calcule du nouveau lambda pour tous les sommets
+            for (int i = 0; i < villes.length; i++) {
+                /* TODO  Implementer la consigne
 				 * double increment = alpha * (arbre.borneSupp() - borne)
 				 * * (arbre.degSommet(i) - 2) / somme;
 				 * System.out.println("Increment : " + increment);
 				 * arbre.incrementLambda(i, increment);
-				 */
-				arbre.incrementLambda(i, arbre.degSommet(i) - 2);
-			}
+                 */
+                arbre.incrementLambda(i, arbre.degSommet(i) - 2);
+            }
 
-			// Indique si l'arbre represente une tournee
-			estUneTournee = arbre.estUneTournee();
+            // Indique si l'arbre represente une tournee
+            estUneTournee = arbre.estUneTournee();
 
-			// Affiche les information de l'iteration courante
+            // Affiche les information de l'iteration courante
 //			System.out.printf("Borne: %s (etait %s) tournee: %s\n", df
 //					.format(borne), df.format(ancienneBorne),
 //					(estUneTournee ? "oui" : "non"));
-			/*
+            /*
 			 * Si la borne ne monte pas, on change alpha
 			 * TODO Implementer la donnee
 			 * if (borne <= ancienneBorne) {
 			 * alpha *= 0.9;
 			 * System.out.println("Alpha :" + alpha);
 			 * }
-			 */
+             */
+            premiereIteration = false;
+            // Continue tant qu'on a une amelioration ou une solution non-acceptable
+        } while (borne > ancienneBorne || !estUneTournee || premiereIteration);
+        // Renvoie la tournee;
+        return arbre.tournee();
+    }
 
-			premiereIteration = false;
-			// Continue tant qu'on a une amelioration ou une solution non-acceptable
-		} while (borne > ancienneBorne || !estUneTournee || premiereIteration);
-		// Renvoie la tournee;
-		return arbre.tournee();
-	}
-	/**
-	 * Charge un fichier TSP et lance la resolution du laboratoire
-	 * @param args Nom du fichier TSP a charger
-	 */
-	public static void main(String[] args) {
-		// Obtient le nom du fichier TSP
-		String file = "data/xqf131.tsp";
-		if (args.length == 1)
-			file = args[0];
-		if (file == null || file.trim().isEmpty() || args.length > 1) {
-			System.out.println("Vous devez specifier un fichier TSP");
-			return;
-		}
-		// Parse les villes depuis le fichier TSP
-		Ville[] villes;
-		try {
-			villes = Ville.parse(file);
-		} catch (Exception e) {
-			villes = null;
-			e.printStackTrace();
-		}
-		// Quitte en cas d'erreur
-		if (villes == null) {
-			System.err.println("Erreur lors de la lecture du fichier TSP");
-			System.exit(-1);
-			return;
-		}
+    /**
+     * Charge un fichier TSP et lance la resolution du laboratoire
+     *
+     * @param args Nom du fichier TSP a charger
+     */
+    public static void main(String[] args) {
+        // Obtient le nom du fichier TSP
+        String file = "data/xqf131.tsp";
+        if (args.length == 1) {
+            file = args[0];
+        }
+        if (file == null || file.trim().isEmpty() || args.length > 1) {
+            System.out.println("Vous devez specifier un fichier TSP");
+            return;
+        }
+        // Parse les villes depuis le fichier TSP
+        Ville[] villes;
+        try {
+            villes = Ville.parse(file);
+        } catch (Exception e) {
+            villes = null;
+            e.printStackTrace();
+        }
+        // Quitte en cas d'erreur
+        if (villes == null) {
+            System.err.println("Erreur lors de la lecture du fichier TSP");
+            System.exit(-1);
+            return;
+        }
+        for (int i = 0; i < villes.length; i++) {
 
-		// Lance la relaxation
-		Ville tournee[] = relaxation(villes);
+            Ville ville = villes[i];
+            //System.out.println("id : "+ville.getId()+" coord : ("+ ville.x + "," + ville.y+")");
+            for (int j = 0; j<villes.length; j++) {
+                Ville ville1 = villes[j];
+                System.out.println("distance entre ville1 "+ville1.getId()+" et ville2 "+ville.getId()+" : "+ville1.distance(ville));
+                
+            }
+            
 
-		// Affiche la tournee
-		System.out.println("Tournee:");
-		for (Ville v : tournee) {
-			System.out.println(v);
-		}
-	}
+        }
+
+        // Lance la relaxation
+//        Ville tournee[] = relaxation(villes);
+
+        // Affiche la tournee
+//        System.out.println("Tournee:");
+//        for (Ville v : tournee) {
+//            System.out.println(v);
+//        }
+    }
 }
