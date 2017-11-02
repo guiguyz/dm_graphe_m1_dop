d0185e3f5e14d280279ea46b7fe0554b88456be2
Guillaume Drouart <guiguyz@hotmail.fr>
2017-11-02 16:27:37 +0100

    kruskal ok

--- Graphe/src/DeuxApproximation.java
+++ Graphe/src/DeuxApproximation.java
@@ -22,31 +22,31 @@ public class DeuxApproximation {
                     int iplus1 = (i + 1) % tree.size();
                     int jplus1 = (j + 1) % tree.size();
 
-                    // Distance (i, i+1)
-                    double distance1 = tournee[i].distance(tournee[iplus1]);
-
-                    // Distance (j, j+1)
-                    double distance2 = tournee[j].distance(tournee[jplus1]);
-
-                    // Distance (i, j)
-                    double distanceA = tournee[i].distance(tournee[j]);
-
-                    // Distance (i+1, j+1)
-                    double distanceB = tournee[iplus1].distance(tournee[jplus1]);
-
-                    // On regarde s'il y a amelioration
-                    if (distance1 + distance2 > distanceA + distanceB + 1E-10) {
-                        // Inversion des elements entre i + 1 et j
-                        int y = j;
-
-                        for (int x = iplus1; x < y; x++) {
-                            Ville tmp2 = tournee[x];
-                            tournee[x] = tournee[y];
-                            tournee[y] = tmp2;
-                            y--;
-                        }
-                        amelioration = true;
-                    }
+//                    // Distance (i, i+1)
+//                    double distance1 = tournee[i].distance(tournee[iplus1]);
+//
+//                    // Distance (j, j+1)
+//                    double distance2 = tournee[j].distance(tournee[jplus1]);
+//
+//                    // Distance (i, j)
+//                    double distanceA = tournee[i].distance(tournee[j]);
+//
+//                    // Distance (i+1, j+1)
+//                    double distanceB = tournee[iplus1].distance(tournee[jplus1]);
+//
+//                    // On regarde s'il y a amelioration
+//                    if (distance1 + distance2 > distanceA + distanceB + 1E-10) {
+//                        // Inversion des elements entre i + 1 et j
+//                        int y = j;
+//
+//                        for (int x = iplus1; x < y; x++) {
+//                            Ville tmp2 = tournee[x];
+//                            tournee[x] = tournee[y];
+//                            tournee[y] = tmp2;
+//                            y--;
+//                        }
+//                        amelioration = true;
+//                    }
                 }
             }
         }
@@ -54,7 +54,7 @@ public class DeuxApproximation {
         // Calcul de la longueur totale
         double longueurTotale = 0;
         for (int i = 0; i < tree.size(); i++) {
-            longueurTotale += tournee[i].distance(tournee[(i + 1)% tournee.length]);
+//            longueurTotale += tournee[i].distance(tournee[(i + 1)% tournee.length]);
         }
 
         return longueurTotale;
